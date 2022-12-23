a67080f3ae4f8961f3963c9f8305518b16a94108
Ozan Cansel <ozancansel@tutanota.com>
2022-12-23 19:12:54 +0300

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Test"
+which sort
+which printf
+function version_greater_equal() {
+ printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
+}
+HZ_VERSION=4.1.3
+version_greater_equal ${HZ_VERSION} 4.2.0
+echo $?
