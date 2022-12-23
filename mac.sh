7375870204720fe832d0982afbe9631766e74c1c
Ozan Cansel <ozancansel@tutanota.com>
2022-12-23 19:18:44 +0300

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -6,6 +6,6 @@ which printf
 function version_greater_equal() {
  printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
 }
-HZ_VERSION=4.1.3
+HZ_VERSION=4.2.3
 version_greater_equal ${HZ_VERSION} 4.2.0
 echo $?
